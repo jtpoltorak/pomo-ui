2ec35abfb5624ad8f0b94685d8697e5806b9441c
Jon Poltorak <jtpoltorak@gmail.com>
2023-08-26 12:32:46 -0400

    Some add'l styling; animated gradient background

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,62 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', 'roboto condensed', 'sofia sans'],
+        sans: [
+          'comfortaa',
+          'barlow',
+          'jost',
+          'cabin',
+          'poppins',
+          'dm sans',
+          'raleway',
+          'montserrat',
+          'belanosima',
+          'sofia sans',
+          'roboto condensed',
+        ],
+        serif: ['libre baskerville'],
         mono: [
           'roboto mono',
-          'JetBrains Mono',
-          'Victor Mono',
+          'jetbrains mono',
+          'victor mono',
         ],
+      },
+      animation: {
+        'gradient-x': 'gradient-x 10s ease infinite',
+        'gradient-y': 'gradient-y 10s ease infinite',
+        'gradient-xy': 'gradient-xy 10s ease infinite',
+      },
+      keyframes: {
+        'gradient-y': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'center top',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'center center',
+          },
+        },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+        'gradient-xy': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
       },
     },
   },
